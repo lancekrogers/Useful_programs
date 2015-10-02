d2c3a9564dcb8a264c089d950b808217ffddea13
Lance Rogers <lancekincaid1994@gmail.com>
2015-10-02 11:56:03 -0400

    caroverpayment.py created

---
+++ caroverpayment.py
@@ -0,0 +1,18 @@
+import sys
+
+try:
+    cost = int(sys.argv[1])
+    term = int(sys.argv[2])
+    rate = float(sys.argv[3])
+
+    def overpay_for_car(cost, term, rate):
+        original = cost
+        for x in range(term):
+            a = cost * rate
+            cost = cost + a
+        return cost - original
+
+    print("You will pay {} over the original amount of {} for the car with a {} year term and {} interest rate".format(overpay_for_car(cost, term, rate), cost, term, rate))
+
+except:
+    print("Please provide system argments. \n.....................\nFormat: caroverpayment.py cost term interest_rate\n.....................")
