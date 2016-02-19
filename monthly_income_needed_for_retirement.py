78180c07aab58c8625ed044b6884170292c2c0d1
Lance Rogers <lancekincaid1994@gmail.com>
2016-02-19 06:47:34 -0500

    added kwarg finder function

--- monthly_income_needed_for_retirement.py
+++ monthly_income_needed_for_retirement.py
@@ -7,30 +7,60 @@ from sys import argv
 
 # income_needed_to_maintain_current_lifestyle_at_retirement
 
-def income_needed(current_monthly_inc, years=30):
+def income_needed(current_monthly_inc, years=30, inflation_rate=0.025):
 	x = current_monthly_inc
 	for i in range(years):
-		y = x * .025
+		y = x * inflation_rate
 		x = x + y
 	return x
+
+def find_kwargs():
+	"""
+		Returns a dictionary of keyward arguments with their values
+	"""
+	empt_dict = {}
+	for i in range(len(argv)):
+		if argv[i].split("="):
+			t = argv[i].split("=")
+			try:
+				empt_dict[t[0]] = t[1]
+			except IndexError:
+				continue
+		else:
+			pass
+	return empt_dict
+
 
 
 def run_func():
 	x = 0
 	y = 0
+	z = 0
+	kwargs = find_kwargs()
+	for key in kwargs:
+		if key == "inflation_rate":
+			z = float(kwargs[key])
+		elif key == 'years':
+			y = int(kwargs[key])
 	try:
-		x = int(argv[1])
-		y = int(argv[2])
+		if x == 0 and y == 0:
+			x = int(argv[1])
+			if z == 0:
+				y = int(argv[2])
 	except:
 		try:
 			x = int(argv[1])
 		except:
-			raise ValueError("Please enter your current monthly income")
-	if x > 0 and y > 0:
+			print("Please try again with your monthly income")
+	if x > 0 and y > 0 and z > 0:
+		return income_needed(x, years=y, inflation_rate=z)
+	elif x > 0 and y > 0 and z <= 0:
 		return income_needed(x, years=y)
-	elif x > 0 and y <= 0:
+	elif x > 0 and y <= 0 and z <= 0:
 		return income_needed(x)
+	elif x > 0 and y <=0 and z > 0:
+		return income_needed(x, inflation_rate=z)
 	else:
-		print("an error occured")
+		raise Exception("An error has occured")
 
 print(run_func())
